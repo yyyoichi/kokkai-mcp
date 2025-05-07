0ee6bf3351d8ce335dd6899872f9e2a61c74c949
yyyoichi <origamiichiro@gmail.com>
2025-05-07 15:18:40 +0900

    feat: parquetをアップロードするタスクの実装を追加

--- src/task.py
+++ src/task.py
@@ -1,22 +1,58 @@
-
-
-from src.client import Client
+import io
 from src.config import UploadSpeechParquetDependency
-from src.sentence_transformers import encode_text
-from src.storage import StorageClient, get_minio_client
+from src.kokkaiapiclient.models.speech_record import SpeechRecord
+from src.model import SpeechParquetModel
+from src.parquet import SpeechParquet
 
 
-def upload_speech_parquet(deps: UploadSpeechParquetDependency):
-    pass
 
+class UploadSpeechParquetTask:
+    def __init__(self, deps: UploadSpeechParquetDependency):
+        self.deps = deps
 
-if __name__ == "__main__":
-    # 依存関係を定義
-    c = Client()
-    deps = UploadSpeechParquetDependency(
-        api_client=c, 
-        embedding=encode_text, 
-        storage_client=StorageClient(client=get_minio_client("", "", ""), bucket_name=""))
-    # S3にアップロードす
-    upload_speech_parquet(deps)
-    
+    async def run(self, year: int, month: int) -> None:
+        """
+        ひと月分の発言をAPIから取得して、Parquet形式で保存する関数
+        """
+        parquet_buffer = SpeechParquet()
+        async for res in self.deps.api_client.iter_speech(year, month):
+            if res.speech_response is None or res.speech_response.speech_record is None:
+                # 発言がない場合はスキップ
+                continue
+            models: list[SpeechParquetModel] = [] # len == res.speech_record.len
+            sentences: list[str] = [] # len == res.speech_record.len
+            for speech in res.speech_response.speech_record:
+                m = self.map_speech_rec_to_model(speech)
+                models.append(m)
+                sentences.append(speech.speech or "")
+            # 発言内容をベクトル化する
+            embedding = self.deps.embedding(sentences)
+            for i, vec in enumerate(embedding):
+                models[i].speech_vector = vec
+            # Parquetファイルに追加する
+            parquet_buffer.append(models)
+        # Parquetを出力
+        buf = parquet_buffer.getvalue()
+        # Parquetを保存する
+        self.deps.storage_client.upload_parquet(year, month, io.BytesIO(buf))
+
+    @staticmethod
+    def map_speech_rec_to_model(speech: SpeechRecord) -> SpeechParquetModel:
+        """
+        APIのレスポンスをSpeechParquetModelに変換する
+        """
+        # 第x号 '第'と'号'を除去して、intに変換
+        issue = int((speech.issue or "").replace("第", "").replace("号", "")) if speech.issue else 0
+        m = SpeechParquetModel(
+            date=speech.date or "",
+            session=speech.session or 0,
+            name_of_house=speech.name_of_house or "", # type: ignore
+            name_of_meeting=speech.name_of_meeting or "",
+            issue=issue,
+            issue_id=speech.issue_i_d or "",
+            speaker=speech.speaker.string if speech.speaker and speech.speaker.string else "",
+            speech_order=speech.speech_order or 0,
+            speech_id=speech.speech_i_d or "",
+            speech_vector=[],
+        )
+        return m
---
+++ tests/test_tesk.py
@@ -0,0 +1,133 @@
+
+
+import io
+import pandas as pd
+import pytest
+import pandas as pd
+import pyarrow.parquet as pq
+from src.config import UploadSpeechParquetDependency
+from src.kokkaiapiclient.api.speech.speech_get_response import SpeechGetResponse
+from src.kokkaiapiclient.models.name_of_house import NameOfHouse
+from src.kokkaiapiclient.models.speech_record import SpeechRecord
+from src.kokkaiapiclient.models.speech_record_speaker import SpeechRecord_speaker
+from src.kokkaiapiclient.models.speech_response import SpeechResponse
+from src.model import SpeechParquetModel
+from src.task import UploadSpeechParquetTask
+
+
+@pytest.mark.parametrize(
+    "rec_count, expected_rows",
+    [
+        (1, 1),
+        (5, 5),
+        (10, 10),
+    ],
+)
+@pytest.mark.asyncio
+async def test_upload_parquet_task_table(rec_count: int, expected_rows: int):
+    class ApiClientMock:
+        def __init__(self, rec_count: int):
+            self.rec_count = rec_count
+
+        async def iter_speech(self, year: int, month: int):
+            for i in range(self.rec_count):
+                rec = SpeechRecord(
+                    date="2024-01-01",
+                    session=210,
+                    name_of_house=NameOfHouse.衆議院,
+                    name_of_meeting="本会議",
+                    issue=f"第{i+1}号",
+                    issue_i_d="ISSUE_001",
+                    speaker=SpeechRecord_speaker("山田だろう"),
+                    speech_order=2,
+                    speech_i_d="SPEECH_002",
+                    speech="発言内容"
+                )
+                yield SpeechGetResponse(
+                    speech_response=SpeechResponse(
+                        speech_record=[rec],
+                        number_of_return=1,
+                    ),
+                )
+
+    class StorageClientMock:
+        def __init__(self):
+            self.uploaded_data: pd.DataFrame = pd.DataFrame()
+
+        def upload_parquet(self, year: int, month: int, buffer: io.BytesIO) -> None:
+            table = pq.read_table(buffer) # type: ignore
+            read_df = table.to_pandas() # type: ignore
+            self.uploaded_data = read_df
+
+    storage_client = StorageClientMock()
+    deps = UploadSpeechParquetDependency(
+        api_client=ApiClientMock(rec_count),
+        embedding=lambda x: [[0.1, 0.2, 0.3] for _ in x],
+        storage_client=storage_client,
+    )
+
+    task = UploadSpeechParquetTask(deps)
+
+    await task.run(2024, 1)
+
+    # ストレージに読まれた行数をテスト
+    assert storage_client.uploaded_data.shape[0] == expected_rows
+    
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (SpeechRecord(
+            date="2024-01-01",
+            session=210,
+            name_of_house=NameOfHouse.衆議院,
+            name_of_meeting="本会議",
+            issue="第1号",
+            issue_i_d="ISSUE_001",
+            speaker=SpeechRecord_speaker("山田だろう"),
+            speech_order=2,
+            speech_i_d="SPEECH_002",
+            speech="発言内容"
+        ), SpeechParquetModel(
+            date="2024-01-01",
+            session=210,
+            name_of_house=NameOfHouse.衆議院,
+            name_of_meeting="本会議",
+            issue=1,
+            issue_id="ISSUE_001",
+            speaker="山田だろう",
+            speech_order=2,
+            speech_id="SPEECH_002",
+            speech_vector=[],
+        )),
+        (SpeechRecord(
+            date="2024-01-01",
+            session=210,
+            name_of_house=NameOfHouse.衆議院,
+            name_of_meeting="本会議",
+            issue="第100号",
+            issue_i_d="ISSUE_001",
+            speaker=SpeechRecord_speaker("山田だろう"),
+            speech_order=2,
+            speech_i_d="SPEECH_002",
+            speech="発言内容"
+        ), SpeechParquetModel(
+            date="2024-01-01",
+            session=210,
+            name_of_house=NameOfHouse.衆議院,
+            name_of_meeting="本会議",
+            issue=100,
+            issue_id="ISSUE_001",
+            speaker="山田だろう",
+            speech_order=2,
+            speech_id="SPEECH_002",
+            speech_vector=[],
+        )),
+    ],
+)
+def test_upload_parquet_task_map_speech_rec_to_model(input: SpeechRecord, expected: SpeechParquetModel):
+    """
+    SpeechRecordをSpeechParquetModelに変換するテスト
+    """
+    got = UploadSpeechParquetTask.map_speech_rec_to_model(input)
+    assert got.as_dict() == expected.as_dict()
